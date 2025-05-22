f0776dbd4fdfd51b11fc3991d159c919b0412ab3
Jay Strong <jay.allen.strong@gmail.com>
2025-05-22 06:02:02 -0400

    Fix Selenium driver initialization by adding options for improved performance and stability

--- Section 2 - Browser Automation and Web Scraping/scrape_text_with_selenium.py
+++ Section 2 - Browser Automation and Web Scraping/scrape_text_with_selenium.py
@@ -7,4 +7,5 @@ options.add_argument("disable-dev-shm-usage")
 options.add_argument("no-sandbox")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_argument("disable-blink-features=AutomationControlled")
-driver = wd.Chrome()
+driver = wd.Chrome(options=options)
+
