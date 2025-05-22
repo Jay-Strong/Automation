65e91858908dcc9e47d8aa9a657d5c7cc01b4d15
Jay Strong <jay.allen.strong@gmail.com>
2025-05-22 06:45:09 -0400

    Refactor Selenium driver setup into a dedicated function for improved code organization

--- Section 2 - Browser Automation and Web Scraping/scrape_text_with_selenium.py
+++ Section 2 - Browser Automation and Web Scraping/scrape_text_with_selenium.py
@@ -1,11 +1,22 @@
 from selenium import webdriver as wd
 
-options = wd.ChromeOptions()
-options.add_argument("disable-infobars")
-options.add_argument("start-maximized")
-options.add_argument("disable-dev-shm-usage")
-options.add_argument("no-sandbox")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_argument("disable-blink-features=AutomationControlled")
-driver = wd.Chrome(options=options)
+def get_driver() -> wd.Chrome:
+    options = wd.ChromeOptions() 
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+    driver = wd.Chrome(options=options)
+    driver.get("http://automated.pythonanywhere.com/")
+    return driver
 
+def main() -> str:
+    driver = get_driver()
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+    return element.text
+
+print(main())
+
+# py scrape_text_with_selenium.py
