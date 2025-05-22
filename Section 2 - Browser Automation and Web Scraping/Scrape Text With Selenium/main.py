7892c8b4357c9132106c58897f355566780502ee
Jay Strong <jay.allen.strong@gmail.com>
2025-05-22 13:02:38 -0400

    Refactor Selenium driver setup into a separate module for improved organization and maintainability

--- Section 2 - Browser Automation and Web Scraping/Scrape Text With Selenium/main.py
+++ Section 2 - Browser Automation and Web Scraping/Scrape Text With Selenium/main.py
@@ -1,16 +1,4 @@
-from selenium import webdriver as wd
-
-def get_driver() -> wd.Chrome:
-    options = wd.ChromeOptions() 
-    options.add_argument("disable-infobars")
-    options.add_argument("start-maximized")
-    options.add_argument("disable-dev-shm-usage")
-    options.add_argument("no-sandbox")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_argument("disable-blink-features=AutomationControlled")
-    driver = wd.Chrome(options=options)
-    driver.get("http://automated.pythonanywhere.com/")
-    return driver
+from driver_config import get_driver
 
 def main() -> str:
     driver = get_driver()
@@ -19,4 +7,4 @@ def main() -> str:
 
 print(main())
 
-# py scrape_text_with_selenium.py
+# py main.py
